051f91721f7af33bec43d90814a43aee6d8052fb
En Shih <seanstone5923@gmail.com>
2018-02-16 23:02:04 +0800

    Instancing

--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -10,7 +10,7 @@ out mediump vec2 TexCoord;
 uniform vec2 WindowSize;
 
 void main() {
-    gl_Position = vec4((aPos.x * 100.0) / WindowSize.x, -aPos.y * 100.0 / WindowSize.y, 0.0, 1.0);
+    gl_Position = vec4((aPos.x * 100.0 + float(gl_InstanceID) * 200.0) / WindowSize.x, -aPos.y * 100.0 / WindowSize.y, 0.0, 1.0);
     ourColor = aColor;
     TexCoord = aTexCoord;
 }
