d445e5aafb1ff8e42f364621bb0e5c1a08dcd54c
En Shih <seanstone5923@gmail.com>
2018-02-18 20:58:48 +0800

    Simplify shader

--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -51,56 +51,61 @@ void main() {
         break;
     }
 
-    switch (int(PlaLifeData) >> 4)
+    if (int(PlaLifeData) > 0x00)
     {
-        case 0x0: // Prokaryotes
-        if ((int(PlaLifeData) & 0xF) > 0) Tile = vec2(41 + (int(PlaLifeData) & 0xF) - 1, 16);
-        break;
-        case 0x1: // Eukaryotes
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 17);
-        break;
-        case 0x2: // Radiates
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 18);
-        break;
-        case 0x3: // Arthropods
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 12);
-        break;
-        case 0x4: // Mollusks
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 14);
-        break;
-        case 0x5: // Fish
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 11);
-        break;
-        case 0x6: // Cetaceans
-        Tile = vec2(1 + 41 + (int(PlaLifeData) & 0xF), 1);
-        break;
-        case 0x7: // Trichordates
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 13);
-        break;
-        case 0x8: // Insects
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 15);
-        break;
-        case 0x9: // Amphibians
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 9);
-        break;
-        case 0xA: // Reptiles
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 3);
-        break;
-        case 0xB: // Dinosaurs
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 5);
-        break;
-        case 0xC: // Avians
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 4);
-        break;
-        case 0xD: // Mammals
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 2);
-        break;
-        case 0xE: // Carniferns
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 20);
-        break;
-        case 0xF: // Robots
-        Tile = vec2(41 + (int(PlaLifeData) & 0xF), 19);
-        break;
+        vec2 LifeBaseTile;
+        switch (int(PlaLifeData) >> 4)
+        {
+            case 0x0: // Prokaryotes
+            LifeBaseTile = vec2(40, 16);
+            break;
+            case 0x1: // Eukaryotes
+            LifeBaseTile = vec2(41, 17);
+            break;
+            case 0x2: // Radiates
+            LifeBaseTile = vec2(41, 18);
+            break;
+            case 0x3: // Arthropods
+            LifeBaseTile = vec2(41, 12);
+            break;
+            case 0x4: // Mollusks
+            LifeBaseTile = vec2(41, 14);
+            break;
+            case 0x5: // Fish
+            LifeBaseTile = vec2(41, 11);
+            break;
+            case 0x6: // Cetaceans
+            LifeBaseTile = vec2(42, 1);
+            break;
+            case 0x7: // Trichordates
+            LifeBaseTile = vec2(41, 13);
+            break;
+            case 0x8: // Insects
+            LifeBaseTile = vec2(41, 15);
+            break;
+            case 0x9: // Amphibians
+            LifeBaseTile = vec2(41, 9);
+            break;
+            case 0xA: // Reptiles
+            LifeBaseTile = vec2(41, 3);
+            break;
+            case 0xB: // Dinosaurs
+            LifeBaseTile = vec2(41, 5);
+            break;
+            case 0xC: // Avians
+            LifeBaseTile = vec2(41, 4);
+            break;
+            case 0xD: // Mammals
+            LifeBaseTile = vec2(41, 2);
+            break;
+            case 0xE: // Carniferns
+            LifeBaseTile = vec2(41, 20);
+            break;
+            case 0xF: // Robots
+            LifeBaseTile = vec2(41, 19);
+            break;
+        }
+        Tile = LifeBaseTile + vec2(int(PlaLifeData) & 0xF, 0);
     }
 
     switch (int(PlaData.y) & 0x3F)
